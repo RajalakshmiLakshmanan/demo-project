4ef5395d48aaa56962243d52de0637ba8a36cd69
RajalakshmiLakshmanan <124457464+RajalakshmiLakshmanan@users.noreply.github.com>
2026-06-04 19:09:46 +0530

    Update StarPattern1.java

--- learnArray/StarPattern1.java
+++ learnArray/StarPattern1.java
@@ -18,3 +18,27 @@ public class StarPattern1 {
 
 
 }
+
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int j=i;j<=5;j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+       for(int i=1; i<=n-1;i++){
+           for(int j=1; j<=i;j++){
+               System.out.print(" ");
+           }
+           for(int k=i;k<=n-1;k++){
+               System.out.print(" *");
+           }
+           System.out.println();
+       }
+    }
+}
